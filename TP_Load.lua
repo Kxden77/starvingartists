1f53f571af2415f2b82eb7f19f9342976da51d6c
donutchip <116406156+donutchip@users.noreply.github.com>
2022-11-06 01:17:16 -0300

    Update TP_Load.lua

--- TP_Load.lua
+++ TP_Load.lua
@@ -12,7 +12,7 @@ player = game.Players.LocalPlayer
 ip = "https://mrbombastic.starvdog.repl.co"
 
 sa_instance = Instance.new("StringValue")
-sa_instance.parent = player
+sa_instance.Parent = player
 sa_instance.Value = LocalToken
 
 --Connect
